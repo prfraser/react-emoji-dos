e5ee888208f73192560a33e95a114b2fa945a233
Patrick Smith <patrick@burntcaramel.com>
2017-11-06 13:51:54 +1100

    Refactor to use external state
    This allows us to ask that state questions:
    e.g. How many items are currently incomplete?

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 import TodoItem from './components/TodoItem'
 
+const items = [
+  { description: 'First', completed: true },
+  { description: 'Second', completed: false },
+  { description: 'Third', completed: true }
+]
+
+const incompletedCount = (items) => items.filter((item) => item.completed == false).length
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <TodoItem completed={ true } description='First' />
-        <TodoItem completed={ false } description='Second' />
-        <TodoItem completed={ true } description='Third' />
+        {
+          items.map((item, index) => (
+            <TodoItem key={ index } { ...item } />
+          ))
+        }
+        <p>{ incompletedCount(items) } remaining</p>
       </div>
     );
   }
