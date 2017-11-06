54c944a3ce5eaa6c1e18c2b7371a6f1a9a5c3cf9
Patrick Smith <patrick@burntcaramel.com>
2017-11-06 14:00:26 +1100

    Move items to App state

--- src/App.js
+++ src/App.js
@@ -2,21 +2,43 @@ import React, { Component } from 'react';
 import './App.css';
 import TodoItem from './components/TodoItem'
 
-const items = [
-  { description: 'First', completed: true },
-  { description: 'Second', completed: false },
-  { description: 'Third', completed: true }
-]
-
-const incompletedCount = (items) => items.filter((item) => item.completed == false).length
+const incompletedCount = (items) => items.filter((item) => item.completed === false).length
+const toggleItemAtIndex = (items, indexToToggle) => {
+  const item = items[indexToToggle]
+  item.completed = !item.completed
+  // items[indexToToggle] = { ...item, completed: !item.completed }
+  return items
+}
 
 class App extends Component {
+  state = {
+    items: [
+      { description: 'First', completed: true },
+      { description: 'Second', completed: false },
+      { description: 'Third', completed: true }
+    ]
+  }
+
+  onToggleItemAtIndex = (index) => {
+    this.setState((prevState) => {
+      let items = prevState.items
+      items = toggleItemAtIndex(items, index)
+      return { items }
+    })
+  }
+
   render() {
+    const items = this.state.items
+
     return (
       <div className="App">
         {
           items.map((item, index) => (
-            <TodoItem key={ index } { ...item } />
+            <TodoItem
+              key={ index }
+              { ...item }
+              onToggle={ () => this.onToggleItemAtIndex(index) }
+            />
           ))
         }
         <p>{ incompletedCount(items) } remaining</p>
